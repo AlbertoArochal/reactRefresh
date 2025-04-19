a14fe9d0753336ca81fdab37e3882782991130c5
AlbertoArochal <arochaldev@gmail.com>
2025-04-19 12:39:33 +0200

    adding message

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,10 +1,18 @@
 import { Counter } from "./Counter";
+import PropTypes from "prop-types";
 
-export function App() {
+export const App = ({ title, subtitle }) => {
   return (
     <div className="App">
       <h1>Hola mundo!!!</h1>
+      <h2 className="props">{title}</h2>
+      <h2 className="props">{subtitle}</h2>
       <Counter />
     </div>
   );
-}
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./HelloWorld.jsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <App title={123} subtitle="amago" />
   </React.StrictMode>
 );
