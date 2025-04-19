278bf21ca1b9b5c5ace09fc640ad92ccf4eea6c7
AlbertoArochal <arochaldev@gmail.com>
2025-04-19 17:10:56 +0200

    adding handlers to add, substract and reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,19 +4,23 @@ import { useState } from "react";
 export const CounterApp = ({ number }) => {
   const [count, setCount] = useState(number);
 
-  const substract = () => setCount((prev) => prev - 1);
-  const add = () => setCount((prev) => prev + 1);
+  const handleSubstract = () => setCount((prevCount) => prevCount - 1);
+  const handleAdd = () => setCount((prevCount) => prevCount + 1);
+  const handleReset = () => setCount(number);
 
   return (
     <>
       {" "}
       <h1 className="app">CounterApp</h1>
       <h2>{count}</h2>
-      <button className="substract" onClick={substract}>
+      <button className="substract" onClick={handleSubstract}>
         -1
       </button>
-      <button className="add" onClick={add}>
+      <button className="add" onClick={handleAdd}>
         +1
+      </button>
+      <button className="reset" onClick={handleReset}>
+        Reset
       </button>
     </>
   );
